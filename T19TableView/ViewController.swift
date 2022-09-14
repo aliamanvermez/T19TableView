b7689436ca700920c223c3d0b1a43c5d315b53f8
aliamanvermez <maliamanvermez@gmail.com>
2022-09-14 10:22:15 +0300

    Initial Commit

---
+++ T19TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T19TableView
+//
+//  Created by NeonApps on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
